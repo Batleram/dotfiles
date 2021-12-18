b9b7545cf3bb38b0cd65c466fc7ba2867e29690f
batleram <batleram@gmail.com>
2021-12-18 00:32:55 -0500

    setup lspsaga and cmp properly enough

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,21 +3,28 @@ local keymap = vim.api.nvim_set_keymap
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
     Plug 'preservim/nerdtree'
+
+    -- THEMES
     --   Plug 'sainnhe/sonokai'
     Plug 'sainnhe/edge'
-    Plug 'tmsvg/pear-tree'
-    --lsp
+    Plug 'windwp/nvim-autopairs'
+    -- LSP
     Plug 'neovim/nvim-lspconfig'
-    Plug 'glepnir/lspsaga.nvim'
     Plug 'tree-sitter/tree-sitter'
+    Plug 'tami5/lspsaga.nvim'
     
+    -- COMPLETION
     Plug 'hrsh7th/nvim-cmp'
     Plug 'hrsh7th/cmp-nvim-lsp'
     Plug 'hrsh7th/cmp-buffer'
-    Plug 'hrsh7th/cmp-vsnip'
-    Plug 'hrsh7th/vim-vsnip'
+    Plug 'L3MON4D3/LuaSnip'
+    Plug 'saadparwaiz1/cmp_luasnip'
 
+    -- LINTING
     Plug 'dense-analysis/ale'
+
+    -- QOL
+    Plug 'tpope/vim-fugitive'
 vim.call('plug#end')
 
 -- apply color scheme
@@ -41,19 +48,34 @@ lspconfig.tsserver.setup{
 } -- js + ts `npm install -g typescript typescript-language-server`
 lspconfig.rust_analyzer.setup{} -- rust `pacman -S rust-analyzer
 lspconfig.bashls.setup{} -- bash `yay -S bash-language-server`
+lspconfig.csharp_ls.setup{} -- dotnet tool install --global csharp-ls and add: export PATH="$PATH:/home/me/.dotnet/tools" to .*shrc
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
 
--- setup lsp saga
-local saga = require('lspsaga')
-saga.init_lsp_saga()
---hover doc
-keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", {silent=true,noremap = true})
+-- Setup lspsaga
+local saga = require 'lspsaga'
+saga.init_lsp_saga{
+    code_action_keys = {
+        quit="<c-c>",
+    },
+    rename_action_keys = {
+        exec="<tab>",
+    },
+}
+keymap('n', 'K', ':Lspsaga hover_doc<CR>',{noremap = true, silent=true})
+keymap('n', '<a-r>', '<cmd>Lspsaga rename<cr>',{noremap = true, silent=true})
+keymap('n', '<a-cr>', ':Lspsaga code_action<CR>', {noremap = true, silent=true})
+keymap('v', '<a-cr>', ':Lspsaga range_code_action<CR>', {noremap = true, silent=true})
 
 -- setup nvim-compe
 local cmp = require'cmp'
 cmp.setup({
      snippet = {
       expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
+        require("luasnip").lsp_expand(args.body)
       end,
     },
     mapping = {
@@ -61,22 +83,27 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ behaviour = cmp.ConfirmBehavior.Insert, select = true }),
+        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {"i","s",}),
+        ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), {"i","s",}),
     },
     sources = {
         { name = 'nvim_lsp' },
         { name = 'buffer' },
-        { name = 'vsnip' }
+        { name = 'luasnip' }
     }
 })
+-- parentheses pair
+require('nvim-autopairs').setup{}
+cmp.event:on( 'confirm_done', require("nvim-autopairs.completion.cmp").on_confirm_done({  map_char = { tex = '' } }))
 
 -- ale config
 vim.g.ale_fix_on_save = 1
 vim.g.ale_fixers = {"prettier",  "black", "rustfmt"}
 vim.g.ale_javascript_prettier_options = '--tab-width 4'
 
--- pear tree
-vim.g.pear_tree_repeatable_expand = 0
+
+
 
 -- disable wrap
 vim.wo.wrap = false
