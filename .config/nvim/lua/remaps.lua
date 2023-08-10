2e0b4012ca3e18f00170035de7eb5ebc90f0ebf9
batleram <batleram@gmail.com>
2023-08-09 22:06:11 -0400

    what

--- .config/nvim/lua/remaps.lua
+++ .config/nvim/lua/remaps.lua
@@ -1,14 +1,14 @@
 -- To find a mapping syntax, do `:map ^v <mapping>` where <mapping> is your mapping
-local keymap = vim.api.nvim_set_keymap
-require('lspsaga').setup({})
+local keymap = vim.keymap.set
 
 vim.cmd("let mapleader = ' '")
 
 -- lspasaga
-keymap('n', 'K', '<cmd>Lspsaga hover_doc<CR>', { noremap = true, silent = true })
 keymap('n', '<leader>r', '<cmd>Lspsaga rename<cr>', { noremap = true, silent = true })
 keymap('n', '<leader><CR>', '<cmd>Lspsaga code_action<CR>', { noremap = true, silent = true })
 keymap('v', '<leader><CR>', '<cmd>Lspsaga range_code_action<CR>', { noremap = true, silent = true })
+keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>", { noremap = true, silent = true })
+keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { noremap = true, silent = true })
 
 -- y acts like d and c, it yoinks to end of line
 keymap('n', 'Y', 'y$', { noremap = true })
@@ -21,16 +21,21 @@ keymap('i', '<c-BS>', '<c-\\><c-o>db', { noremap = true })
 
 -- format text
 function FormatRange()
-  vim.lsp.buf.format({
-    async = true,
-    range = {
-      ["start"] = vim.api.nvim_buf_get_mark(0, "<"),
-      ["end"] = vim.api.nvim_buf_get_mark(0, ">"),
-    }
-  })
+    vim.lsp.buf.format({
+        async = true,
+        range = {
+            ["start"] = vim.api.nvim_buf_get_mark(0, "<"),
+            ["end"] = vim.api.nvim_buf_get_mark(0, ">"),
+        }
+    })
 end
+
 keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<cr><cmd>w<cr>', { noremap = true })
 keymap('v', '<leader>f', '<cmd>lua FormatRange()<cr><cmd>w<cr>', { noremap = true })
+
+-- center on chunk down
+keymap('n', '}', '}zz', { noremap = true })
+keymap('n', '{', '{zz', { noremap = true })
 
 -- reload vimrc
 keymap('n', '<leader>so', '<cmd>so $MYVIMRC<cr>', { noremap = true })
@@ -45,3 +50,9 @@ require('nvim_comment').setup({
     comment_empty = false
 })
 
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -180,6 +180,11 @@ _G.packer_plugins = {
     path = "/home/me/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-treesitter-context"] = {
+    loaded = true,
+    path = "/home/me/.local/share/nvim/site/pack/packer/start/nvim-treesitter-context",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
+  },
   ["nvim-ts-autotag"] = {
     loaded = true,
     path = "/home/me/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
@@ -195,10 +200,25 @@ _G.packer_plugins = {
     path = "/home/me/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/home/me/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
   sonokai = {
     loaded = true,
     path = "/home/me/.local/share/nvim/site/pack/packer/start/sonokai",
     url = "https://github.com/sainnhe/sonokai"
+  },
+  ["telescope-fzf-native.nvim"] = {
+    loaded = true,
+    path = "/home/me/.local/share/nvim/site/pack/packer/start/telescope-fzf-native.nvim",
+    url = "https://github.com/nvim-telescope/telescope-fzf-native.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/home/me/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["uwu.vim"] = {
     loaded = true,
