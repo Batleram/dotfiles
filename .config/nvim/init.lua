374a68930884d4870488fcdc91af3eacee991e3c
Batleram <batleram@gmail.com>
2024-07-23 10:51:22 -0400

    Move header insertion thing so it is actually called

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,3 +11,9 @@ local lsp = require('lsp-zero').preset({
 lsp.nvim_workspace()
 
 lsp.setup()
+
+-- lsp configurations
+local clang_path = vim.fs.normalize(vim.api.nvim_eval("stdpath('data')") .. '/mason/bin/clangd', {})
+require('lspconfig').clangd.setup({
+    cmd = { clang_path, '-header-insertion=never'}
+})
